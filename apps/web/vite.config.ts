ad8709d3589ce6ba5e3508b49a6e6cafeb95fc25
Dave Martinez <34991082+dkm-coder@users.noreply.github.com>
2026-03-23 13:00:59 +0100

    Add base URL for vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,15 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/",
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
-    conditions: ['worker'],
+    conditions: ["worker"],
   },
-})
+});
